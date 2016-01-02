a2ebbeed7322c3db0e5dd903418645cdfbb7ccb0
Kacper Kaliński <kaqukal@gmail.com>
2016-01-02 14:36:47 +0100

    Application Client and Server base

---
+++ src/pl/kaqu/pg/client/PGApplication.java
@@ -0,0 +1,40 @@
+package pl.kaqu.pg.client;
+
+/*
+    PuzzleGenerals
+    Copyright (C) 2016 kaqu
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PGApplication extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //TODO - create application
+    }
+
+    public static void main(String[] args) {
+        System.out.println("PuzzleGenerals, Copyright (C) kaqu\n" +
+                " PuzzleGenerals comes with ABSOLUTELY NO WARRANTY.\n" +
+                " This is free software, and you are welcome to redistribute it\n" +
+                " under certain conditions;");
+        Application.launch(args);
+    }
+}
