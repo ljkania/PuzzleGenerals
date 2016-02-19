c391170424b0244e3122084f9ce0f3ca0ae7b73b
LJK <kanialj@gmail.com>
2016-02-19 16:42:57 +0100

    Refactor of PGPlayerAreaTest

--- src/pl/kaqu/pg/engine/gamearea/PGPlayerAreaTest.java
+++ src/pl/kaqu/pg/engine/gamearea/PGPlayerAreaTest.java
@@ -1,6 +1,8 @@
 package pl.kaqu.pg.engine.gamearea;
 
 import org.jetbrains.annotations.NotNull;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -44,24 +46,40 @@ import static org.junit.Assert.*;
  */
 
 public class PGPlayerAreaTest {
+    static int width;
+    static int height;
+    static PGPlayer player;
+    PGPlayerArea playerArea;
+
     @Rule
     public final ExpectedException exception = ExpectedException.none();
+
+    @BeforeClass
+    public static void init() {
+        width = 8;
+        height = 6;
+        player = new PGPlayer();
+    }
+
+    @Before
+    public void initPlayerArea() {
+        playerArea = new PGPlayerArea(width, height, player);
+    }
 
     @Test
     public void constructor_FirstArgumentIncorrect_IllegalArgumentExceptionThrown() {
         exception.expect(IllegalArgumentException.class);
-        new PGPlayerArea(0, 6, new PGPlayer());
+        new PGPlayerArea(0, height, player);
     }
 
     @Test
     public void constructor_SecondArgumentIncorrect_IllegalArgumentExceptionThrown() {
         exception.expect(IllegalArgumentException.class);
-        new PGPlayerArea(8, -1, new PGPlayer());
+        new PGPlayerArea(width, -1, player);
     }
 
     @Test
     public void getField_AnyCorrectArguments_ArgumentsEqualsReturnedPGFieldCoordinate() throws PGOutOfAreaException {
-        PGPlayerArea playerArea = new PGPlayerArea(8, 6, new PGPlayer());
         int x = 2;
         int y = 5;
         PGField field = playerArea.getField(x, y);
@@ -71,7 +89,6 @@ public class PGPlayerAreaTest {
 
     @Test
     public void getField_GetTwoFieldsNextToEachOther_FieldsShouldBeHorizontalNeighbours() throws PGOutOfAreaException {
-        PGPlayerArea playerArea = new PGPlayerArea(8, 6, new PGPlayer());
         PGField field1 = playerArea.getField(3, 4);
         PGField field2 = playerArea.getField(4, 4);
         assertEquals(field1.getRightNeighbor(), field2);
@@ -80,7 +97,6 @@ public class PGPlayerAreaTest {
 
     @Test
     public void getField_GetOneFieldInFrontOfAnother_FieldsShouldBeVerticalNeighbours() throws PGOutOfAreaException {
-        PGPlayerArea playerArea = new PGPlayerArea(8, 6, new PGPlayer());
         PGField field1 = playerArea.getField(1, 4);
         PGField field2 = playerArea.getField(1, 5);
         assertEquals(field1.getRearNeighbor(), field2);
@@ -89,49 +105,42 @@ public class PGPlayerAreaTest {
 
     @Test
     public void getField_GetFieldNextToLeftEdge_LeftNeighbourShouldBeNull() throws PGOutOfAreaException {
-        PGPlayerArea playerArea = new PGPlayerArea(8, 6, new PGPlayer());
         PGField field = playerArea.getField(0, 4);
         assertNull(field.getLeftNeighbor());
     }
 
     @Test
     public void getField_GetFieldNextToRightEdge_RightNeighbourShouldBeNull() throws PGOutOfAreaException {
-        PGPlayerArea playerArea = new PGPlayerArea(8, 6, new PGPlayer());
         PGField field = playerArea.getField(7, 4);
         assertNull(field.getRightNeighbor());
     }
 
     @Test
      public void getField_GetFieldAtFirstRow_FrontNeighbourShouldBeNull() throws PGOutOfAreaException {
-        PGPlayerArea playerArea = new PGPlayerArea(8, 6, new PGPlayer());
         PGField field = playerArea.getField(3, 0);
         assertNull(field.getFrontNeighbor());
     }
 
     @Test
      public void getField_GetFieldAtLastRow_RearNeighbourShouldBeNull() throws PGOutOfAreaException {
-        PGPlayerArea playerArea = new PGPlayerArea(8, 6, new PGPlayer());
         PGField field = playerArea.getField(4, 5);
         assertNull(field.getRearNeighbor());
     }
 
     @Test
     public void getField_GetFieldAtLastRow_SecondRearNeighbourShouldBeNull() throws PGOutOfAreaException {
-        PGPlayerArea playerArea = new PGPlayerArea(8, 6, new PGPlayer());
         PGField field = playerArea.getField(3, 5);
         assertNull(field.getSecondRearNeighbor());
     }
 
     @Test
     public void getField_GetFieldAtOneBeforeLastRow_SecondRearNeighbourShouldBeNull() throws PGOutOfAreaException {
-        PGPlayerArea playerArea = new PGPlayerArea(8, 6, new PGPlayer());
         PGField field = playerArea.getField(2, 4);
         assertNull(field.getSecondRearNeighbor());
     }
 
     @Test
     public void getField_GetAnyFieldAndFieldTwoSpaceBehindIt_SecondOneEqualsFirstSecondNeighbour() throws PGOutOfAreaException {
-        PGPlayerArea playerArea = new PGPlayerArea(8, 6, new PGPlayer());
         PGField field1 = playerArea.getField(5, 1);
         PGField field2 = playerArea.getField(5, 3);
         assertEquals(field1.getSecondRearNeighbor(), field2);
@@ -139,45 +148,33 @@ public class PGPlayerAreaTest {
 
     @Test
     public void getField_FirstArgumentNegative_PGOutOfAreaExceptionThrown() throws PGOutOfAreaException {
-        PGPlayerArea playerArea = new PGPlayerArea(8, 6, new PGPlayer());
         exception.expect(PGOutOfAreaException.class);
         playerArea.getField(-1, 4);
     }
 
     @Test
     public void getField_FirstArgumentTooBig_PGOutOfAreaExceptionThrown() throws PGOutOfAreaException {
-        PGPlayerArea playerArea = new PGPlayerArea(8, 6, new PGPlayer());
         exception.expect(PGOutOfAreaException.class);
         playerArea.getField(8, 4);
     }
 
     @Test
     public void getField_SecondArgumentNegative_PGOutOfAreaExceptionThrown() throws PGOutOfAreaException {
-        PGPlayerArea playerArea = new PGPlayerArea(8, 6, new PGPlayer());
         exception.expect(PGOutOfAreaException.class);
         playerArea.getField(0, -2);
     }
 
     @Test
     public void getField_SecondArgumentTooBig_PGOutOfAreaExceptionThrown() throws PGOutOfAreaException {
-        PGPlayerArea playerArea = new PGPlayerArea(8, 6, new PGPlayer());
         exception.expect(PGOutOfAreaException.class);
+
         playerArea.getField(4, 7);
     }
 
     @Test
     public void moveUnitToReserve_OnePGUnitSmallOnTheGridMovedToReserve_ReserveCounterIncremented() throws PGOutOfAreaException, PGIncorrectUnitLocationException {
-        int width = 8;
-        int height = 6;
-        PGPlayer player = new PGPlayer();
-        PGPlayerArea playerArea = new PGPlayerArea(width, height, player);
         int sizeOfReserve = playerArea.getReserve().sizeOfReserve();
-        long unitId = 0;
-        PGUnitGroup group = null;
-        PGUnitState state = PGUnitState.IDLE;
-        PGUnitContainer primaryContainer = playerArea.getField(3,4);
-
-        PGUnit unit = new PGUnitSmall(unitId, player, group, state, primaryContainer) {
+        PGUnit unit = new PGUnitSmall(0, player, null, PGUnitState.IDLE, playerArea.getField(3,4)) {
             @Override
             public void activate(@NotNull PGActivationType activationType) {
 
@@ -229,21 +226,13 @@ public class PGPlayerAreaTest {
         };
 
         playerArea.moveUnitToReserve(unit);
+
         assertEquals(sizeOfReserve + 1, playerArea.getReserve().sizeOfReserve());
     }
 
     @Test
     public void moveUnitToReserve_OnePGUnitSmallOnTheGridMovedToReserve_UnitHasClearedUnitContainers() throws PGOutOfAreaException, PGIncorrectUnitLocationException {
-        int width = 8;
-        int height = 6;
-        PGPlayer player = new PGPlayer();
-        PGPlayerArea playerArea = new PGPlayerArea(width, height, player);
-        long unitId = 0;
-        PGUnitGroup group = null;
-        PGUnitState state = PGUnitState.IDLE;
-        PGUnitContainer primaryContainer = playerArea.getField(3,4);
-
-        PGUnit unit = new PGUnitSmall(unitId, player, group, state, primaryContainer) {
+        PGUnit unit = new PGUnitSmall(0, player, null, PGUnitState.IDLE, playerArea.getField(3,4)) {
             @Override
             public void activate(@NotNull PGActivationType activationType) {
 
@@ -295,21 +284,14 @@ public class PGPlayerAreaTest {
         };
 
         playerArea.moveUnitToReserve(unit);
+
         assertTrue(unit.getCurrentUnitContainers().isEmpty());
     }
 
     @Test
     public void moveUnitToReserve_OnePGUnitSmallOnTheGridMovedToReserve_GridDoesNotContainUnitAnymore() throws PGOutOfAreaException, PGIncorrectUnitLocationException {
-        int width = 8;
-        int height = 6;
-        PGPlayer player = new PGPlayer();
-        PGPlayerArea playerArea = new PGPlayerArea(width, height, player);
-        long unitId = 0;
-        PGUnitGroup group = null;
-        PGUnitState state = PGUnitState.IDLE;
         PGUnitContainer primaryContainer = playerArea.getField(3,4);
-
-        PGUnit unit = new PGUnitSmall(unitId, player, group, state, primaryContainer) {
+        PGUnit unit = new PGUnitSmall(0, player, null, PGUnitState.IDLE, primaryContainer) {
             @Override
             public void activate(@NotNull PGActivationType activationType) {
 
@@ -361,22 +343,14 @@ public class PGPlayerAreaTest {
         };
 
         playerArea.moveUnitToReserve(unit);
+
         assertNull(primaryContainer.getContainedUnit());
     }
 
     @Test
     public void moveUnitToReserve_BiggerUnitOnTheGridMovedToReserve_ReserveCounterIncremented() throws PGOutOfAreaException, PGIncorrectUnitLocationException {
-        int width = 8;
-        int height = 6;
-        PGPlayer player = new PGPlayer();
-        PGPlayerArea playerArea = new PGPlayerArea(width, height, player);
         int sizeOfReserve = playerArea.getReserve().sizeOfReserve();
-        long unitId = 0;
-        PGUnitGroup group = null;
-        PGUnitState state = PGUnitState.IDLE;
-        PGUnitContainer primaryContainer = playerArea.getField(1,3);
-
-        PGUnit unit = new PGUnitLarge(unitId, player, group, state, primaryContainer) {
+        PGUnit unit = new PGUnitLarge(0, player, null, PGUnitState.IDLE, playerArea.getField(1,3)) {
             @Override
             public void activate(@NotNull PGActivationType activationType) {
 
@@ -428,21 +402,13 @@ public class PGPlayerAreaTest {
         };
 
         playerArea.moveUnitToReserve(unit);
+
         assertEquals(sizeOfReserve + 1, playerArea.getReserve().sizeOfReserve());
     }
 
     @Test
     public void moveUnitToReserve_BiggerUnitOnTheGridMovedToReserve_UnitHasClearedUnitContainers() throws PGOutOfAreaException, PGIncorrectUnitLocationException {
-        int width = 8;
-        int height = 6;
-        PGPlayer player = new PGPlayer();
-        PGPlayerArea playerArea = new PGPlayerArea(width, height, player);
-        long unitId = 0;
-        PGUnitGroup group = null;
-        PGUnitState state = PGUnitState.IDLE;
-        PGUnitContainer primaryContainer = playerArea.getField(1,3);
-
-        PGUnit unit = new PGUnitLarge(unitId, player, group, state, primaryContainer) {
+        PGUnit unit = new PGUnitLarge(0, player, null, PGUnitState.IDLE, playerArea.getField(1,3)) {
             @Override
             public void activate(@NotNull PGActivationType activationType) {
 
@@ -494,21 +460,14 @@ public class PGPlayerAreaTest {
         };
 
         playerArea.moveUnitToReserve(unit);
+
         assertTrue(unit.getCurrentUnitContainers().isEmpty());
     }
 
     @Test
     public void moveUnitToReserve_BiggerUnitOnTheGridMovedToReserve_GridDoesNotContainUnitAnymore() throws PGOutOfAreaException, PGIncorrectUnitLocationException {
-        int width = 8;
-        int height = 6;
-        PGPlayer player = new PGPlayer();
-        PGPlayerArea playerArea = new PGPlayerArea(width, height, player);
-        long unitId = 0;
-        PGUnitGroup group = null;
-        PGUnitState state = PGUnitState.IDLE;
         PGField primaryContainer = playerArea.getField(1,3);
-
-        PGUnit unit = new PGUnitLarge(unitId, player, group, state, primaryContainer) {
+        PGUnit unit = new PGUnitLarge(0, player, null, PGUnitState.IDLE, primaryContainer) {
             @Override
             public void activate(@NotNull PGActivationType activationType) {
 
@@ -560,6 +519,7 @@ public class PGPlayerAreaTest {
         };
 
         playerArea.moveUnitToReserve(unit);
+
         assertNull(primaryContainer.getContainedUnit());
         assertNull(primaryContainer.getRightNeighbor().getContainedUnit());
         assertNull(primaryContainer.getRearNeighbor().getContainedUnit());
